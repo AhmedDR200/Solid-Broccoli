312343ecd7a78c274cc746dd9a038bede4b935cb
AhmedDR200 <alshwwhy212@gmail.com>
2023-10-16 18:59:44 +0300

    Using Function Based Views For Create Retrieve or List

--- product/views.py
+++ product/views.py
@@ -1,7 +1,11 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -20,3 +24,22 @@ class ProductCreate(generics.CreateAPIView):
 class ProductList(generics.ListCreateAPIView):
      queryset = Product.objects.all().order_by('id')[:4]
      serializer_class = ProductSerializer
+
+
+
+@api_view(['GET','POST'])
+def product_alt_view(request,pk=None ,*args, **kwargs):
+     method = request.method
+     if method == "GET":
+          if pk is not None:
+               queryset = Product.objects.filter(pk=pk)
+               if not queryset.exist():
+                    raise Http404
+               return Response()
+          
+          queryset = Product.objects.all()
+          data = ProductSerializer(queryset, many=True).data
+          return Response(data)
+     
+     if method == "POST":
+         pass
